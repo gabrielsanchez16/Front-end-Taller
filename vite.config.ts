0b50d59c0541e42e9e95f9b00f578b2aab0d2c66
GaboDev <gabrielpelota.8@gmail.com>
2025-08-21 20:04:46 -0500

    changes finals

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // o "/tu-subcarpeta/" si tu app no está en root
   optimizeDeps: {
     include: ["quill", "react-quilljs"]
   },
