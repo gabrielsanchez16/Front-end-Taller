58a63e2a32e8ad90f8e3f2bf4f5d70e65942de4c
GaboDev <gabrielpelota.8@gmail.com>
2025-08-21 19:58:34 -0500

    changes finals

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["quill", "react-quilljs"]
+  },
+  build: {
+    commonjsOptions: {
+      include: [/quill/, /react-quilljs/, /node_modules/]
+    }
+  }
 })
